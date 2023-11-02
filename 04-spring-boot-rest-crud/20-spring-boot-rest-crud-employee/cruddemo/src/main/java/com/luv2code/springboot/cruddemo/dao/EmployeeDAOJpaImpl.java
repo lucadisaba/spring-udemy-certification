1ca0e47736c81ae9ed1b49b9eb7d6755e383ed18
lucadisaba <lucadisabatino97@gmail.com>
2023-11-02 14:54:25 +0100

    REST crud operations with jpaRepository last commit

---
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,51 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO{
+
+    // define field for entity manager
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
+
+        List<Employee> employees = theQuery.getResultList();
+
+        return employees;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = entityManager.merge(theEmployee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        entityManager.remove(theEmployee);
+    }
+
+
+
+
+}
---
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,72 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+
+import com.luv2code.springboot.cruddemo.service.EmployeeServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    // expose "/employees" and return a list of employees
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employ id - " + employeeId;
+    }
+
+}
