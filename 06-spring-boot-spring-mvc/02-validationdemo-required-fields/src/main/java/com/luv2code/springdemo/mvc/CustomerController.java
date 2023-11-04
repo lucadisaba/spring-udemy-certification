444c4dc946138938f832099df580417d67338d13
lucadisaba <lucadisabatino97@gmail.com>
2023-11-04 20:24:55 +0100

    Validation form data wth Spring last commit

---
+++ 06-spring-boot-spring-mvc/02-validationdemo-required-fields/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springdemo.mvc;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping ("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+
+        // Utilizzato per il debugging del codice errore del campo nella validazione, noi infatti
+        // abbiamo sovrascritto il codice errore con qualcosa di più esplicativo --> Invalid number
+
+        // System.out.println("Binding results: " + theBindingResult.toString());
+
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
+    }
+
+}
